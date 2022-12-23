cc4e0ea58bffaf92360795e62723f3344a7f95a3
RiyaSontakke <riyaharhare04@gmail.comgit config --global user.email riyaharhare04@gmail.com>
2022-12-23 08:21:30 +0530

    Added_Welcome_Msg

---
+++ WageComputation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.empwage;
+
+public class WageComputation {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+	}
+
+}
