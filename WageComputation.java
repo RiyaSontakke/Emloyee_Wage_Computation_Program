60f99038c7b5ddf4a2b6c0b5ad7db422f524dc74
RiyaSontakke <riyaharhare04@gmail.comgit config --global user.email riyaharhare04@gmail.com>
2022-12-23 13:36:08 +0530

    Added:part time employee and Wage

--- WageComputation.java
+++ WageComputation.java
@@ -3,24 +3,32 @@ package com.bridgelabz.empwage;
 public class WageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		// constants
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME_ = 2 ;
 		int EMP_RATE_PER_HOUR = 20;
+		//variables
 		int empHrs = 0;
 		int empWage = 0;
+		// computation
 		double empcheck = Math.floor(Math.random() * 10) % 2;
-		System.out.println(+empcheck);
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		System.out.println(empCheck);
+		
 		if(empcheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
-			empHrs = 8;
+			
+			if (empCheck == IS_PART_TIME)
+				empHrs = 4;
 		}
-		else
-		{
-			System.out.println("Employee is Absent");
-			empHrs = 0;
-		}
+			else if (empCheck == IS_FULL_TIME_)
+			{
+				empHrs = 8;
+			}
+		
+			else
+				empHrs = 0;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage: " +empWage);
-	
-
 }
 }
