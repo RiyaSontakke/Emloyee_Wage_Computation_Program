abe49739389021beebd65fede3afba426cd8dec5
RiyaSontakke <riyaharhare04@gmail.comgit config --global user.email riyaharhare04@gmail.com>
2022-12-23 11:42:04 +0530

    Added: Daily Employee Wage

--- WageComputation.java
+++ WageComputation.java
@@ -4,12 +4,23 @@ public class WageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int IS_FULL_TIME = 1;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		System.out.println(empCheck);
-		if(empCheck == IS_FULL_TIME)
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		System.out.println(+empcheck);
+		if(empcheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present");
+			empHrs = 8;
+		}
 		else
+		{
 			System.out.println("Employee is Absent");
-	}
+			empHrs = 0;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " +empWage);
+	
 
+}
 }
