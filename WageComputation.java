bf12631769a3727bde4b8da43dbab4f741b5fe0f
RiyaSontakke <riyaharhare04@gmail.comgit config --global user.email riyaharhare04@gmail.com>
2022-12-26 13:18:49 +0530

    Added:Wages for a Month

--- WageComputation.java
+++ WageComputation.java
@@ -4,38 +4,33 @@ public class WageComputation {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static void main(String[] args) {
-		
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int empHrs = 0;
+		 int empHrs = 0;
 		 int empWage = 0;
-		 int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-		
-		    System.out.println(empCheck);
-		
-		switch (empCheck) {
-		case IS_PART_TIME:
-		
-            System.out.println(empWage);
-			empHrs = 4;
-			break;
-		
-		case IS_FULL_TIME:		
-			System.out.println(empWage);
-		    empHrs = 8;
-		    break;
-		default :
-		
-			System.out.println(empWage);
-			empHrs = 0;
-		    break;
-		}
+		 int totalEmpWage = 0;
+		    for(int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		    	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		    	switch (empCheck) {
+		    	case IS_PART_TIME:
+		    		empHrs= 4;
+		    		break;
+		    	case IS_FULL_TIME:
+		    		empHrs = 8;
+		    		break;
+		    	default:
+		    		empHrs = 0;
+		    	}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage:" + empWage);		
-		}
-	}
+		totalEmpWage += empWage;
+		    }
+		    empWage = empHrs * EMP_RATE_PER_HOUR;
+		    System.out.println("Emp wage:" +empWage);
+		    System.out.println("Total Emp Wage:" +totalEmpWage);		
+	}		
+}
 
-	
 		
 	
 
